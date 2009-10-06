d0fb07318e1fd1f762f8fdaea590b9057b1d2be1
bsavelev <bsavelev>
2009-10-06 09:35:33 +0000

    remove CC

--- epm.mk
+++ epm.mk
@@ -1,17 +1,13 @@
 EPM_LINUX_LDFLAGS=-Wl,-rpath,/opt/drweb/lib
 EPM_BSD_LDFLAGS=-Wl,-rpath,/usr/local/drweb/lib
 EPM_SUN_LDFLAGS=-Wl,-R,/opt/drweb/lib
-DEFAULT_CC=gcc
-DEFAULT_CXX=g++
-BSD_CC=gcc
-BSD_CXX=g++
 
 BUILD_EPM_CONF_STRING_COMMON = LDFLAGS=-Wl,-R,lib
 BUILD_EPM_CONF_STRING_DEFAULT = $(BUILD_EPM_CONF_STRING_COMMON) --with-softwaredir=/etc/drweb/software
 BUILD_EPM_CONF_STRING_BSD = $(BUILD_EPM_CONF_STRING_COMMON) --with-softwaredir=/usr/local/etc/drweb/software
 
 configure-epm-bsd: Makefile
-	cd epm && ./configure CC=$(BSD_CC) CXX=$(BSD_CXX) $(BUILD_EPM_CONF_STRING_BSD)
+	cd epm && ./configure $(BUILD_EPM_CONF_STRING_BSD)
 	touch $@
 
 configure-epm-default: Makefile
@@ -34,13 +30,13 @@ BUILD_FLTK_CONF_STRING_COMMON = --prefix=$(CURDIR)/epm/fltk-install --enable-loc
 BUILD_EPM_FLTK_ADD_STRING = FLTKCONFIG=$(CURDIR)/epm/fltk-install/bin/fltk-config --enable-gui
 
 build-fltk-bsd:
-	cd epm/fltk && ( ./configure CC=$(BSD_CC) CXX=$(BSD_CXX) $(BUILD_FLTK_CONF_STRING_COMMON) ; $(MAKE) install )
+	cd epm/fltk && ( ./configure $(BUILD_FLTK_CONF_STRING_COMMON) ; $(MAKE) install )
 
 build-fltk-default:
 	cd epm/fltk && ( ./configure $(BUILD_FLTK_CONF_STRING_COMMON) ; $(MAKE) install )
 
 build-epm-gui-bsd: build-fltk-bsd
-	cd epm && ( [ -f Makefile ] || ./configure CC=$(BSD_CC) CXX=$(BSD_CXX) $(BUILD_EPM_CONF_STRING_BSD) $(BUILD_EPM_FLTK_ADD_STRING) ; $(MAKE) )
+	cd epm && ( [ -f Makefile ] || ./configure $(BUILD_EPM_CONF_STRING_BSD) $(BUILD_EPM_FLTK_ADD_STRING) ; $(MAKE) )
 
 build-epm-gui-default: build-fltk-default
 	cd epm && ( [ -f Makefile ] || ./configure $(BUILD_EPM_CONF_STRING_DEFAULT) $(BUILD_EPM_FLTK_ADD_STRING) ; $(MAKE) )

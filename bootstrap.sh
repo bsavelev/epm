8d63fc00ba78ecf8976a2181297d7fb1a717552d
Boris Savelev <boris@altlinux.org>
2010-02-10 13:57:28 +0300

    rewrite configure; fix NLS autotools

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,24 @@
 # $Id: bootstrap.sh,v 1.2.16.4 2009/10/27 16:08:20 bsavelev Exp $
 #
 
+old(){
+autopoint -f
+rm po/Makevars.template
+rm po/Rules-quot
+rm po/boldquot.sed
+rm po/en@boldquot.header
+rm po/en@quot.header
+rm po/insert-header.sin
+rm po/quot.sed
+
 aclocal --force && \
-autoconf
+autoconf --force && autoheader && automake --foreign --add-missing --copy --force
+}
+
+
+	rm -f ./install.sh  aclocal.m4
+	autoheader
+	aclocal
+	autoconf
+	automake -a -c --foreign
+	touch po/remove-potcdate.sin

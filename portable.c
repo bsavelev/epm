00599a64c0a49d6ec9e04f6537f39ab7148afeb3
Boris Savelev <boris@altlinux.org>
2011-01-27 17:23:24 +0300

    fix update-rc.d call for remove

--- portable.c
+++ portable.c
@@ -3079,7 +3079,7 @@ write_remove(dist_t     *dist,		/* I - Software distribution */
 	fputs("	if test -x /sbin/chkconfig; then\n", scriptfile);
 	qprintf(scriptfile, "\t\t/sbin/chkconfig --del %s >/dev/null 2>&1 || true\n", file->dst);
 	fputs("	elif ( test -x \"`which update-rc.d 2>/dev/null`\" >/dev/null 2>&1 ); then\n", scriptfile);
-	qprintf(scriptfile, "\t\tupdate-rc.d -f %s >/dev/null 2>&1 || true\n", file->dst);
+	qprintf(scriptfile, "\t\tupdate-rc.d -f %s remove >/dev/null 2>&1 || true\n", file->dst);
 	fputs("	else\n", scriptfile);
 	for (runlevels = get_runlevels(dist->files + i, "0235");
              isdigit(*runlevels & 255);
